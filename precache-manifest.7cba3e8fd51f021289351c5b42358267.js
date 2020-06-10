71ec58e167c6135ba5b5efad64b8d90f1588642f
cuongdnv <cuongdnv@nissho-vn.com>
2020-06-10 20:02:20 +0700

    Updates

---
+++ precache-manifest.7cba3e8fd51f021289351c5b42358267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20eb3223aebb01fde1699ca29d6e032f",
+    "url": "/computershop/index.html"
+  },
+  {
+    "revision": "b3bd9be20bd7b702ef1d",
+    "url": "/computershop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "11aac22552698d5160a7",
+    "url": "/computershop/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "b3bd9be20bd7b702ef1d",
+    "url": "/computershop/static/js/2.c367a204.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/computershop/static/js/2.c367a204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11aac22552698d5160a7",
+    "url": "/computershop/static/js/main.70a348c1.chunk.js"
+  },
+  {
+    "revision": "cfc2a1a2db97933d4596",
+    "url": "/computershop/static/js/runtime-main.f2961093.js"
+  }
+]);
